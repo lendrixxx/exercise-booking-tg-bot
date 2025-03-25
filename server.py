1fe93d057ce1fcb315d1a26fd6a506783ba979e7
Lendrix <65599524+lendrixxx@users.noreply.github.com>
2025-03-25 09:23:57 +0800

    Update server to get render base url instead of using hardcoded value

--- server.py
+++ server.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Dummy Server is running"
+  return 'Dummy Server is running'
 
-@app.route("/health", methods=["GET"])
+@app.route('/health', methods=['GET'])
 def health_check():
-  return "OK", 200  
+  return 'OK', 200  
 
 def ping_dummy_server():
-  DUMMY_SERVER_URL = "https://exercise-booking-tg-bot.onrender.com/health"  
+  DUMMY_SERVER_URL = f'{os.getenv('RENDER_EXTERNAL_URL', 'http://localhost:5000')}/health'
   try:
     response = requests.get(DUMMY_SERVER_URL)
     if response.status_code == 200:
-      global_variables.LOGGER.info("Successfully pinged the dummy server.")
+      global_variables.LOGGER.info(f'Successfully pinged the dummy server {DUMMY_SERVER_URL}.')
     else:
-      global_variables.LOGGER.warning(f"Unexpected response from server: {response.status_code}")
+      global_variables.LOGGER.warning(f'Unexpected response from server: {response.status_code}')
   except requests.exceptions.RequestException as e:
-    global_variables.LOGGER.error(f"Failed to reach dummy server: {e}")
+    global_variables.LOGGER.error(f'Failed to reach dummy server: {e}')
 
 def start_server():
   app.run(host='0.0.0.0', port=5000)
